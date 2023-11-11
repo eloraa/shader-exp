95dc824614fef577cc86f9dc57f8651e3a169648
elora <108570316+eloraistired@users.noreply.github.com>
2023-11-11 13:11:09 +0000

    added the animated favicon

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,7 +1,9 @@
+import { animateFavicon } from '../utils/favicon';
 import { initTheme } from './theme';
 export const APP = {};
 
 export const init = () => {
   initTheme();
+  animateFavicon()
 };
 
---
+++ src/js/utils/favicon.js
@@ -0,0 +1,74 @@
+
+export const animateFavicon = () => {
+  const fileLink = document.querySelector('link[rel="icon"]').href;
+
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d', { willReadFrequently: true }); 
+
+
+  const video = document.createElement('video');
+
+  video.src = fileLink;
+  video.crossOrigin = 'anonymous'; 
+  video.style.display = 'none';
+  document.body.appendChild(video);
+
+  let animationStarted = false;
+
+  function isYellowish(r, g, b) {
+    const redThreshold = 150;
+    const greenThreshold = 150;
+    const blueThreshold = 100;
+    return r > redThreshold && g > greenThreshold && b < blueThreshold;
+  }
+
+  function draw() {
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+      const red = data[i];
+      const green = data[i + 1];
+      const blue = data[i + 2];
+
+      if (isYellowish(red, green, blue)) {
+        data[i + 3] = 0;
+      }
+    }
+
+    context.putImageData(imageData, 0, 0);
+
+    if (video.currentTime >= video.duration) {
+      video.currentTime = 0;
+    }
+
+    const dataURL = canvas.toDataURL('image/png');
+
+    const favicon = document.querySelector('link[rel="icon"]');
+    if (favicon) {
+      favicon.href = dataURL;
+    }
+
+    requestAnimationFrame(draw);
+  }
+
+  document.addEventListener(
+    'click',
+    function () {
+      if (!animationStarted) {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+
+        video.play();
+        draw();
+
+        animationStarted = true;
+      }
+    },
+    { once: true }
+  );
+
+  video.load();
+};
